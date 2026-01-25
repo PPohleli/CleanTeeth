73162f19ff9664a9b1f61af0767e4d5d5da1dc60
pohleliphiwe <pohleliphiwe@gmail.com>
2026-01-25 12:28:23 +0200

    Adding application Layer and creating first Command: CreateDentalOfficeCommand.

---
+++ CleanTeeth.Application/Features/DentalOffices/Commands/CreateDentalOffice/CreateDentalOfficeCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanTeeth.Application.Features.DentalOffices.Commands.CreateDentalOffice
+{
+    public class CreateDentalOfficeCommandHandler
+    {
+        public async Task<Guid> Handle (CreateDentalOfficeCommand command)
+        {
+            // Implementation to create a dental office
+            throw new NotImplementedException();
+        }
+    }
+}
