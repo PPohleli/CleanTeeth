d34c197d5c6f9e1fc9d1a0297b203ca85ca86705
pohleliphiwe <pohleliphiwe@gmail.com>
2026-08-15 17:48:15 +0200

    Add first Command: Creating a DentalOffice

---
+++ CleanTeeth.Application/Features/DentalOffices/Commands/CreateDentalOffice/CreateDentalOfficeCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanTeeth.Application.Features.DentalOffices.Commands.CreateDentalOffice
+{
+    public class CreateDentalOfficeCommand
+    {
+        public required string Name { get; set; }
+    }
+}
---
+++ CleanTeeth.Application/Features/DentalOffices/Commands/CreateDentalOffice/CreateDentalOfficeCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanTeeth.Application.Features.DentalOffices.Commands.CreateDentalOffice
+{
+    public class CreateDentalOfficeCommandHandler
+    {
+        public async Task<Guid> Handle(CreateDentalOfficeCommand command)
+        {
+            // Implement the logic to create a dental office here
+
+            throw new NotImplementedException();
+        }
+    }
+}
