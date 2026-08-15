d241b3e94e378d0ed2265417c1c4976859e59719
pohleliphiwe <pohleliphiwe@gmail.com>
2026-08-15 18:52:42 +0200

    Validating the Application Layer: Introduce FluentValidation.

--- CleanTeeth.Application/Features/DentalOffices/Commands/CreateDentalOffice/CreateDentalOfficeCommandHandler.cs
+++ CleanTeeth.Application/Features/DentalOffices/Commands/CreateDentalOffice/CreateDentalOfficeCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿using CleanTeeth.Application.Contracts.Persistence;
 using CleanTeeth.Application.Contracts.Repositories;
 using CleanTeeth.Domain.Entities;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,15 +14,23 @@ namespace CleanTeeth.Application.Features.DentalOffices.Commands.CreateDentalOff
     {
         private readonly IDentalOfficeRepository repository;
         private readonly IUnitOfWork unitOfWork;
+        private readonly IValidator<CreateDentalOfficeCommand> validator;
 
-        public CreateDentalOfficeCommandHandler(IDentalOfficeRepository repository, IUnitOfWork unitOfWork)
+        public CreateDentalOfficeCommandHandler(IDentalOfficeRepository repository, IUnitOfWork unitOfWork, IValidator<CreateDentalOfficeCommand> validator)
         {
             this.repository = repository;
             this.unitOfWork = unitOfWork;
+            this.validator = validator;
         }
         public async Task<Guid> Handle(CreateDentalOfficeCommand command)
         {
             // Implement the logic to create a dental office here
+            var validationResult = await validator.ValidateAsync(command);
+
+            if (!validationResult.IsValid)
+            {
+                // Throw an exception or return an error response indicating validation failure
+            }
 
             var dentalOffice = new DentalOffice(command.Name);
 
