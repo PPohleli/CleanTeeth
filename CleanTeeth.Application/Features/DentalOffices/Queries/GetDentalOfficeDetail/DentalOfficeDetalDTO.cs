c73a3f808b93173a9fea15903ef376221768a75c
pohleliphiwe <pohleliphiwe@gmail.com>
2026-08-15 22:12:06 +0200

    Creating first Query: GetDentalOfficeDetailQuery, DentalOfficeDetalDTO, GetDentalOfficeDetailQueryHandler and NotFoundException

---
+++ CleanTeeth.Application/Features/DentalOffices/Queries/GetDentalOfficeDetail/DentalOfficeDetalDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanTeeth.Application.Features.DentalOffices.Queries.GetDentalOfficeDetail
+{
+    public class DentalOfficeDetalDTO
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
---
+++ CleanTeeth.Application/Features/DentalOffices/Queries/GetDentalOfficeDetail/GetDentalOfficeDetailQuery.cs
@@ -0,0 +1,14 @@
+﻿using CleanTeeth.Application.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanTeeth.Application.Features.DentalOffices.Queries.GetDentalOfficeDetail
+{
+    public class GetDentalOfficeDetailQuery: IRequest<DentalOfficeDetalDTO>
+    {
+        public required Guid Id { get; set; }
+    }
+}
